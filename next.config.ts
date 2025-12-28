7460a26d39ebfc5f01a2ca6e0b92d629ddc5c966
reecert <tuscanoreece12@gmail.com>
2025-12-28 12:45:41 -0600

    minor deployment issue update

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "export",
   images: { unoptimized: true },
+
+  // Set the base path for GitHub Pages deployment (subdirectory)
+  // CHANGE THIS if your repository name is different!
+  basePath: "/portfolio-sakura",
 };
 
 export default nextConfig;
