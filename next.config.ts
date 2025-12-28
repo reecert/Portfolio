b59bf599157a958b1d235ce472eb171eeebcf103
reecert <tuscanoreece12@gmail.com>
2025-12-28 12:52:32 -0600

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,18 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+// CHANGE THIS if your repository name is different!
+const repoName = "Portfolio";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  output: "export",
+  output: isGithubActions ? "export" : undefined,
   images: { unoptimized: true },
 
-  // Set the base path for GitHub Pages deployment (subdirectory)
-  // CHANGE THIS if your repository name is different!
-  basePath: "/portfolio-sakura",
+  // Set the base path ONLY for GitHub Pages deployment
+  basePath: isGithubActions ? `/${repoName}` : undefined,
 };
 
 export default nextConfig;
